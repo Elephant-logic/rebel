11aba56b472c8a9d0b8814b2924691a755eb0596
Elephant_logic <keith.davies1978@gmail.com>
2026-01-10 06:40:40 +0000

    Update server.js
    Signed-off-by: Elephant_logic <keith.davies1978@gmail.com>

--- server.js
+++ server.js
@@ -118,4 +118,4 @@ io.on('connection', (socket) => {
 
 server.listen(PORT, '0.0.0.0', () => {
   console.log(`Rebel Messenger server listening on port ${PORT}`);
-});; full code was given above
+});;
