7de78b6f8032587c2bcaef679304280136ab324e
Elephant_logic <keith.davies1978@gmail.com>
2026-01-11 04:04:06 +0000

    Update server.js
    Signed-off-by: Elephant_logic <keith.davies1978@gmail.com>

--- server.js
+++ server.js
@@ -10,33 +10,21 @@ const io = new Server(server, { cors: { origin: "*" } });
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-const rooms = new Map();
-
 io.on('connection', (socket) => {
-  console.log('Connected:', socket.id);
-
   socket.on('join-room', ({ room, name }) => {
     socket.join(room);
     socket.data.room = room;
-    socket.data.name = name || 'User';
-    
-    // THE TRIGGER: Tell everyone else in room that someone joined
-    socket.to(room).emit('user-joined', socket.id); 
-    
-    console.log(`${name} joined ${room}`);
+    // Tell Host a user is here so they can start the stream
+    socket.to(room).emit('user-joined', socket.id);
   });
 
-  // Simple Relay for WebRTC Signals
+  // Relay Calls
   socket.on('webrtc-offer', (data) => socket.to(data.room).emit('webrtc-offer', data));
   socket.on('webrtc-answer', (data) => socket.to(data.room).emit('webrtc-answer', data));
   socket.on('webrtc-ice-candidate', (data) => socket.to(data.room).emit('webrtc-ice-candidate', data));
   
-  // Chat & Files
+  // Relay Chat
   socket.on('chat-message', (data) => io.to(data.room).emit('chat-message', data));
-
-  socket.on('disconnect', () => {
-    console.log('Disconnected:', socket.id);
-  });
 });
 
 server.listen(PORT, '0.0.0.0', () => console.log(`Server running on ${PORT}`));
