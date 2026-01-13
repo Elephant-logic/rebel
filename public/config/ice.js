ecfcf3790d267c65b57d0ab87a0cd0279763f065
Elephant_logic <keith.davies1978@gmail.com>
2026-01-13 18:39:45 +0000

    Update ice.js
    Signed-off-by: Elephant_logic <keith.davies1978@gmail.com>

--- public/config/ice.js
+++ public/config/ice.js
@@ -1,12 +1,20 @@
-// ICE / STUN / TURN config
-// For serious use, replace TURN with your own credentials.
-
 const ICE_SERVERS = [
   { urls: 'stun:stun.l.google.com:19302' },
   { urls: 'stun:stun1.l.google.com:19302' },
+  // OpenRelay (Free Tier) - Vital for Mobile 4G/5G Support
   {
-    urls: 'turn:relay.metered.ca:80',
-    username: 'openai-demo',
-    credential: 'openai-demo'
+    urls: 'turn:openrelay.metered.ca:80',
+    username: 'openrelayproject',
+    credential: 'openrelayproject'
+  },
+  {
+    urls: 'turn:openrelay.metered.ca:443',
+    username: 'openrelayproject',
+    credential: 'openrelayproject'
+  },
+  {
+    urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+    username: 'openrelayproject',
+    credential: 'openrelayproject'
   }
 ];
